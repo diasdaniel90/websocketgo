2b3c9a581b685207d24694fe5021be604cbf9d18
danieldiasconcert <daniel.dias@concert.com.br>
2023-03-22 14:48:51 -0300

    ajuste struct

--- func_msg.go
+++ func_msg.go
@@ -9,12 +9,30 @@ var last_id string = "0"
 var last_id_waiting string = "0"
 
 type Payload struct {
-	ID        string `json:"id"`
-	Color     int    `json:"color"`
-	Roll      int    `json:"roll"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-	Status    string `json:"status"`
+	IdBet                string  `json:"id"`
+	Color                int     `json:"color"`
+	Roll                 int     `json:"roll"`
+	CreatedAt            string  `json:"created_at"`
+	UpdatedAt            string  `json:"updated_at"`
+	Status               string  `json:"status"`
+	TotalRedEurBet       float64 `json:"total_red_eur_bet"`
+	TotalRedBetsPlaced   int     `json:"total_red_bets_placed"`
+	TotalWhiteEurBet     float64 `json:"total_white_eur_bet"`
+	TotalWhiteBetsPlaced int     `json:"total_white_bets_placed"`
+	TotalBlackEurBet     float64 `json:"total_black_eur_bet"`
+	TotalBlackBetsPlaced int     `json:"total_black_bets_placed"`
+	Bets                 []Bet   `json:"bets"`
+}
+
+type Bet struct {
+	IDBets       string  `json:"id"`
+	Color        int     `json:"color"`
+	Amount       float32 `json:"amount"`
+	CurrencyType string  `json:"currency_type"`
+	Status       string  `json:"status"`
+	User         struct {
+		IDStr string `json:"id_str"`
+	} `json:"user"`
 }
 
 func decodePayload(message []byte) (*Payload, error) {
